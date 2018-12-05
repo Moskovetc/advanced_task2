8f3594461862591831c1689daec915f096e5eb59
moskovetc <moskovetc@hotmail.com>
2018-12-05 16:54:35 +0400

    resolve

--- src/main/java/service/Transaction.java
+++ src/main/java/service/Transaction.java
@@ -2,10 +2,16 @@ package service;
 
 import accounts.Account;
 
+<<<<<<< HEAD
 import java.util.concurrent.locks.ReentrantLock;
 
 public class Transaction {
     private Account fromAccount;
     private Account toAccount;
     private ReentrantLock lock;
+=======
+public class Transaction {
+    private Account fromAccount;
+    private Account toAccount;
+>>>>>>> initial
 }
